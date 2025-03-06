1952e9943dc167b057d5a3b7f91dc2fa24c25b4a
Angyalka-Baracskai <77279133+Angyalka-Baracskai@users.noreply.github.com>
2025-03-06 12:32:21 +0100

    Add files via upload
    ujabb verzio

--- DATABASE/magantanar.sql
+++ DATABASE/magantanar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3307
--- Létrehozás ideje: 2025. Feb 20. 15:04
+-- Létrehozás ideje: 2025. Már 06. 12:32
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -32,26 +32,25 @@ CREATE TABLE `diak` (
   `d_nev` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `jelszo` varchar(255) DEFAULT NULL,
-  `tanar_id` int(11) DEFAULT NULL,
-  `tantargy_id` int(11) DEFAULT NULL
+  `aktiv` enum('true','false') NOT NULL DEFAULT 'false'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `diak`
 --
 
-INSERT INTO `diak` (`diak_id`, `d_nev`, `email`, `jelszo`, `tanar_id`, `tantargy_id`) VALUES
-(1, 'Kiss Anna', 'anna.kiss@example.com', 'jelszo123', 1, 1),
-(2, 'Nagy Gábor', 'gabor.nagy@example.com', 'jelszo124', 2, 2),
-(3, 'Szabó Júlia', 'julia.szabo@example.com', 'jelszo125', 3, 3),
-(4, 'Horváth Mária', 'maria.horvath@example.com', 'jelszo126', 4, 4),
-(5, 'Tóth Péter', 'peter.toth@example.com', 'jelszo127', 5, 5),
-(6, 'Mészáros Zoltán', 'zoltan.meszaros@example.com', 'jelszo128', 6, 6),
-(7, 'Horváth Ádám', 'adam.horvath@example.com', 'jelszo129', 7, 7),
-(8, 'Tóth Klára', 'klara.toth@example.com', 'jelszo130', 8, 8),
-(9, 'Kovács Norbert', 'norbert.kovacs@example.com', 'jelszo131', 9, 9),
-(10, 'Nagy Zsófia', 'zsofia.nagy@example.com', 'jelszo132', 10, 10),
-(11, 'Péter Katalin', 'katalin.peter@example.com', 'jelszo133', 11, 11);
+INSERT INTO `diak` (`diak_id`, `d_nev`, `email`, `jelszo`, `aktiv`) VALUES
+(1, 'Kiss Anna', 'anna.kiss@example.com', 'jelszo123', 'true'),
+(2, 'Nagy Gábor', 'gabor.nagy@example.com', 'jelszo124', 'false'),
+(3, 'Szabó Júlia', 'julia.szabo@example.com', 'jelszo125', ''),
+(4, 'Horváth Mária', 'maria.horvath@example.com', 'jelszo126', ''),
+(5, 'Tóth Péter', 'peter.toth@example.com', 'jelszo127', ''),
+(6, 'Mészáros Zoltán', 'zoltan.meszaros@example.com', 'jelszo128', ''),
+(7, 'Horváth Ádám', 'adam.horvath@example.com', 'jelszo129', ''),
+(8, 'Tóth Klára', 'klara.toth@example.com', 'jelszo130', ''),
+(9, 'Kovács Norbert', 'norbert.kovacs@example.com', 'jelszo131', ''),
+(10, 'Nagy Zsófia', 'zsofia.nagy@example.com', 'jelszo132', ''),
+(11, 'Péter Katalin', 'katalin.peter@example.com', 'jelszo133', '');
 
 -- --------------------------------------------------------
 
@@ -89,25 +88,45 @@ CREATE TABLE `tanar` (
   `bszamla` int(11) DEFAULT NULL,
   `adoszam` varchar(255) DEFAULT NULL,
   `IBAN` varchar(255) DEFAULT NULL,
-  `tantargy_id` int(11) DEFAULT NULL
+  `aktiv` enum('true','false') NOT NULL DEFAULT 'false'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `tanar`
 --
 
-INSERT INTO `tanar` (`tanar_id`, `t_nev`, `iranyitoszam`, `varos`, `utca`, `hazszam`, `email`, `jelszo`, `telefonszam`, `dijszabas`, `bemutatkozas`, `bszamla`, `adoszam`, `IBAN`, `tantargy_id`) VALUES
-(1, 'Nagy Péter', '1011', 'Budapest', 'Fő utca', '1', 'peter.nagy@example.com', 'tanar123', '+36123456789', '5000 ', 'Tapasztalt matematika tanár.', 12345678, '12345678-1-42', 'HU12345678901234567890123456', 1),
-(2, 'Szabó László', '1022', 'Budapest', 'Történelem utca', '3', 'laszlo.szabo@example.com', 'tanar456', '+36123456790', '4500 ', 'Fizika tanár és kutató.', 23456789, '23456789-2-43', 'HU23456789012345678901234567', 2),
-(3, 'Kovács Eszter', '1033', 'Budapest', 'Kémia utca', '5', 'eszter.kovacs@example.com', 'tanar789', '+36123456791', '4800 ', 'Kémia szakértő.', 34567890, '34567890-3-44', 'HU34567890123456789012345678', 3),
-(4, 'Horváth Zoltán', '1044', 'Budapest', 'Biológia utca', '7', 'zoltan.horvath@example.com', 'tanar101', '+36123456792', '4700 ', 'Biológia tanár.', 45678901, '45678901-4-45', 'HU45678901234567890123456789', 4),
-(5, 'Tóth Anita', '1055', 'Budapest', 'Irodalom utca', '9', 'anita.toth@example.com', 'tanar112', '+36123456793', '4000 ', 'Történelem tanár és mentor.', 56789012, '56789012-5-46', 'HU56789012345678901234567890', 5),
-(6, 'Mészáros Katalin', '1066', 'Budapest', 'Matematika utca', '12', 'katalin.meszaros@example.com', 'tanar113', '+36123456794', '5300 ', 'Matematika és statisztika tanár.', 67890123, '67890123-6-47', 'HU67890123456789012345678901', 6),
-(7, 'Juhász Ferenc', '1077', 'Budapest', 'Fizikai utca', '13', 'ferenc.juhasz@example.com', 'tanar114', '+36123456795', '4600 ', 'Fizikai kutató és tanár.', 78901234, '78901234-7-48', 'HU78901234567890123456789012', 7),
-(8, 'Balogh Zsófia', '1088', 'Budapest', 'Zsófia utca', '15', 'balogh.zsofia@example.com', 'tanar115', '+36123456796', '4700 ', 'Matematika és statisztika előadó.', 89012345, '', 'HU89012345678901234567890123', 8),
-(9, 'Varga Béla', '1099', 'Budapest', 'Béla utca', '17', 'bela.varga@example.com', 'tanar116', '+36123456797', '4800 ', 'Biológia és ökológia oktató.', 90123456, '90123456-9-50', 'HU90123456789012345678901234', 9),
-(10, 'Kiss Judit', '1101', 'Budapest', 'Judit utca', '19', 'judit.kiss@example.com', 'tanar117', '+36123456798', '4900 ', 'Történelem és politika szakértő.', 12345678, '12345678-10-51', 'HU12345678901234567890123456', 10),
-(11, 'Farkas Tamás', '1102', 'Budapest', 'Műszaki utca', '21', 'tamas.farkas@example.com', 'tanar118', '+36123456799', '5000 ', 'Műszaki ismeretek tanár.', 11223344, '11223344-11-52', 'HU11223344556677889900123456', 11);
+INSERT INTO `tanar` (`tanar_id`, `t_nev`, `iranyitoszam`, `varos`, `utca`, `hazszam`, `email`, `jelszo`, `telefonszam`, `dijszabas`, `bemutatkozas`, `bszamla`, `adoszam`, `IBAN`, `aktiv`) VALUES
+(1, 'Nagy Péter', '1011', 'Budapest', 'Fő utca', '1', 'peter.nagy@example.com', 'tanar123', '+36123456789', '5000 ', 'Tapasztalt matematika tanár.', 12345678, '12345678-1-42', 'HU12345678901234567890123456', 'true'),
+(2, 'Szabó László', '1022', 'Budapest', 'Történelem utca', '3', 'laszlo.szabo@example.com', 'tanar456', '+36123456790', '4500 ', 'Fizika tanár és kutató.', 23456789, '23456789-2-43', 'HU23456789012345678901234567', 'false'),
+(3, 'Kovács Eszter', '1033', 'Budapest', 'Kémia utca', '5', 'eszter.kovacs@example.com', 'tanar789', '+36123456791', '4800 ', 'Kémia szakértő.', 34567890, '34567890-3-44', 'HU34567890123456789012345678', ''),
+(4, 'Horváth Zoltán', '1044', 'Budapest', 'Biológia utca', '7', 'zoltan.horvath@example.com', 'tanar101', '+36123456792', '4700 ', 'Biológia tanár.', 45678901, '45678901-4-45', 'HU45678901234567890123456789', ''),
+(5, 'Tóth Anita', '1055', 'Budapest', 'Irodalom utca', '9', 'anita.toth@example.com', 'tanar112', '+36123456793', '4000 ', 'Történelem tanár és mentor.', 56789012, '56789012-5-46', 'HU56789012345678901234567890', ''),
+(6, 'Mészáros Katalin', '1066', 'Budapest', 'Matematika utca', '12', 'katalin.meszaros@example.com', 'tanar113', '+36123456794', '5300 ', 'Matematika és statisztika tanár.', 67890123, '67890123-6-47', 'HU67890123456789012345678901', ''),
+(7, 'Juhász Ferenc', '1077', 'Budapest', 'Fizikai utca', '13', 'ferenc.juhasz@example.com', 'tanar114', '+36123456795', '4600 ', 'Fizikai kutató és tanár.', 78901234, '78901234-7-48', 'HU78901234567890123456789012', ''),
+(8, 'Balogh Zsófia', '1088', 'Budapest', 'Zsófia utca', '15', 'balogh.zsofia@example.com', 'tanar115', '+36123456796', '4700 ', 'Matematika és statisztika előadó.', 89012345, '', 'HU89012345678901234567890123', ''),
+(9, 'Varga Béla', '1099', 'Budapest', 'Béla utca', '17', 'bela.varga@example.com', 'tanar116', '+36123456797', '4800 ', 'Biológia és ökológia oktató.', 90123456, '90123456-9-50', 'HU90123456789012345678901234', ''),
+(10, 'Kiss Judit', '1101', 'Budapest', 'Judit utca', '19', 'judit.kiss@example.com', 'tanar117', '+36123456798', '4900 ', 'Történelem és politika szakértő.', 12345678, '12345678-10-51', 'HU12345678901234567890123456', ''),
+(11, 'Farkas Tamás', '1102', 'Budapest', 'Műszaki utca', '21', 'tamas.farkas@example.com', 'tanar118', '+36123456799', '5000 ', 'Műszaki ismeretek tanár.', 11223344, '11223344-11-52', 'HU11223344556677889900123456', '');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `tanartantargy`
+--
+
+CREATE TABLE `tanartantargy` (
+  `tanar_id` int(11) NOT NULL,
+  `tantargy_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- A tábla adatainak kiíratása `tanartantargy`
+--
+
+INSERT INTO `tanartantargy` (`tanar_id`, `tantargy_id`) VALUES
+(8, 4),
+(11, 7),
+(6, 4);
 
 -- --------------------------------------------------------
 
@@ -178,9 +197,7 @@ INSERT INTO `uzenetek` (`uzenetek_id`, `datum`, `diak_id`, `tanar_id`, `szoveg`)
 -- A tábla indexei `diak`
 --
 ALTER TABLE `diak`
-  ADD PRIMARY KEY (`diak_id`),
-  ADD KEY `tantargy_id` (`tantargy_id`),
-  ADD KEY `tanar_id` (`tanar_id`);
+  ADD PRIMARY KEY (`diak_id`);
 
 --
 -- A tábla indexei `esemeny`
@@ -197,11 +214,17 @@ ALTER TABLE `tanar`
   ADD PRIMARY KEY (`tanar_id`);
 
 --
+-- A tábla indexei `tanartantargy`
+--
+ALTER TABLE `tanartantargy`
+  ADD KEY `fk_tanartantargy` (`tanar_id`),
+  ADD KEY `fk_tantargy` (`tantargy_id`);
+
+--
 -- A tábla indexei `tantargyak`
 --
 ALTER TABLE `tantargyak`
-  ADD PRIMARY KEY (`tantargy_id`),
-  ADD KEY `tanar_id` (`tanar_id`);
+  ADD PRIMARY KEY (`tantargy_id`);
 
 --
 -- A tábla indexei `uzenetek`
@@ -244,13 +267,6 @@ ALTER TABLE `uzenetek`
 --
 
 --
--- Megkötések a táblához `diak`
---
-ALTER TABLE `diak`
-  ADD CONSTRAINT `diak_ibfk_1` FOREIGN KEY (`tantargy_id`) REFERENCES `tantargyak` (`tantargy_id`),
-  ADD CONSTRAINT `diak_ibfk_2` FOREIGN KEY (`tanar_id`) REFERENCES `tanar` (`tanar_id`);
-
---
 -- Megkötések a táblához `esemeny`
 --
 ALTER TABLE `esemeny`
@@ -259,10 +275,11 @@ ALTER TABLE `esemeny`
   ADD CONSTRAINT `fv_esem_tant` FOREIGN KEY (`tantargy_id`) REFERENCES `tantargyak` (`tantargy_id`);
 
 --
--- Megkötések a táblához `tantargyak`
+-- Megkötések a táblához `tanartantargy`
 --
-ALTER TABLE `tantargyak`
-  ADD CONSTRAINT `tantargyak_ibfk_1` FOREIGN KEY (`tanar_id`) REFERENCES `tanar` (`tanar_id`);
+ALTER TABLE `tanartantargy`
+  ADD CONSTRAINT `fk_tanartantargy` FOREIGN KEY (`tanar_id`) REFERENCES `tanar` (`tanar_id`),
+  ADD CONSTRAINT `fk_tantargy` FOREIGN KEY (`tantargy_id`) REFERENCES `tantargyak` (`tantargy_id`);
 
 --
 -- Megkötések a táblához `uzenetek`
