c538ec3e6412ee2a4dbcb2b710161fe51a49cf04
P3T8 <peter.maraczi2005.02@gmail.com>
2025-03-14 09:16:44 +0100

    x

--- JS/ROUTES/db.js
+++ JS/ROUTES/db.js
@@ -9,7 +9,11 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'magantanar',
+<<<<<<< HEAD
     port: process.env.DB_PORT || 3306, //sulis gépen ezt a 3307-re átkell írni
+=======
+    port: process.env.DB_PORT || 3307,
+>>>>>>> 89b8372 (x)
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
