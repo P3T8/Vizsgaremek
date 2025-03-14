f639dc02703b6ef5c4d9cdecb155cc5d132f3cd3
P3T8 <77279060+P3T8@users.noreply.github.com>
2025-03-14 10:52:18 +0100

    connect

--- JS/ROUTES/db.js
+++ JS/ROUTES/db.js
@@ -9,7 +9,7 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'magantanar',
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 3306, //sulis gépen ezt a 3307-re átkell írni
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
